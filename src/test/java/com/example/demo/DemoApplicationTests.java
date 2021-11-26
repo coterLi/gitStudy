3eb1dd60de448ff0a1fe3193d4e30d040edf96cf
xiaonan.li <nelson.li@ihr360.com>
2021-11-26 18:57:58 +0800

    导入历史自建指标sql生成代码。

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -8,7 +8,10 @@ class DemoApplicationTests {
 
     @Test
     void contextLoads() {
-        // 修改1
+        // cid:a63b33f6-ead7-46ce-bc77-e3b5fbcbc5f9&fileName=th.jpg
+        // cid:a63b33f6-ead7-46ce-bc77-e3b5fbcbc5f9
+        String str = "<img src=\"cid:a63b33f6-ead7-46ce-bc77-e3b5fbcbc5f9&fileName=th - 啊啊.jpg\" title=\"th.jpg\" alt=\"th.jpg\" />";
+        str = str.replaceAll("(cid:)([a-zA-Z0-9-]*)([^&\\S*\\s*])(\"$)", "$1$2");
+        System.out.println(str);
     }
-
 }
